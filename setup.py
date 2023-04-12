ba8839b4b43d35c844d7c39ee61111927b2e73ee
shankartumma <tshankaritpc@gmail.com>
2023-04-12 15:27:42 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Downloads your favourite music'
+LONG_DESCRIPTION = 'A package that allows to download your favourite music'
+
+# Setting up
+setup(
+    name="melodymaster",
+    version=VERSION,
+    author="Vikranth",
+    author_email="<vikrantht32@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pytube', 'pywhatkit', 'requests', 'moviepy', 'spotipy', 'webbrowser', 'time', 'urllib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
